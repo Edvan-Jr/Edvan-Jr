f46cbf5e986b139eedd7dee3b0c3cc99a5e64a62
Edvan-Jr <edvan.hd@gmail.com>
2022-01-04 08:57:56 -0300

    :construction: Contato

--- eddev/src/App.js
+++ eddev/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Components/Header';
 import Home from './Pages/Home';
 import Sobre from './Pages/Sobre';
+import Contato from './Pages/Contato';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sobre" element={<Sobre />} />
+          <Route path="/contato" element={<Contato />} />
         </Routes>
       </Router>
     </div>
