f36ac5c637a79cccb151b833c5c29c2e294c3ce7
Edvan-Jr <edvan.hd@gmail.com>
2022-01-04 05:44:25 -0300

    :construction: Header

--- eddev/src/App.js
+++ eddev/src/App.js
@@ -1,9 +1,13 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>EdDev</h1>
+      <Router>
+        <Header />
+      </Router>
     </div>
   );
 }
