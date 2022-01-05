03564d692a319a85d4f73b4cb535376a26b34201
Edvan-Jr <edvan.hd@gmail.com>
2022-01-04 23:25:15 -0300

    :construction: Favicon

--- eddev/src/Pages/Sobre/index.js
+++ eddev/src/Pages/Sobre/index.js
@@ -15,10 +15,10 @@ function Sobre() {
           meu pai possuía duas propriedades rurais, onde eu ficava encantado pela natureza. Sendo assim, gosto de combinar a tecnologia com o rústico.
         </p>
         <p>
-          Mas não pude me dedicar às minhas paixões, em meio às circunstâncias da vida, até eu receber uma oferta de bolsa de estudo para um curso profissionalizante Web Dev Full Stack, em 2021, da parceiria iFood + Cubos Academy. Foram 750 horas de aula, numa rotina intensa com dedicação integral, durante 6 meses, aprendendo FrontEnd, Back-End e Soft Skills. Na sequência, o iFood abriu um processo seletivo para admissão no projeto iLab, e depois de três etapas, fui aprovado. O meu sim veio! O match perfeito!
+          Mas não pude me dedicar às minhas paixões, em meio às circunstâncias da vida, até eu receber uma oferta de bolsa de estudo para um curso profissionalizante Web Dev Full Stack, em 2021, da parceiria iFood + Cubos Academy. Foram 750 horas de aula, numa rotina intensa com dedicação integral, durante 6 meses, aprendendo Front-End, Back-End e Soft Skills. Na sequência, o iFood abriu um processo seletivo para admissão no projeto iLab, e depois de três etapas, fui aprovado. O meu sim veio! O match perfeito!
         </p>
         <p>
-          Então, aos 34 anos de idade, recém-casado e com filho de 1 ano, recém-órfão de pai e com uma parte de uma propriedade rural de herança, eis a oportunidade de viver apaixonadamente tech 'n roots, sendo FoodLover.
+          Então, aos 35 anos de idade, recém-casado e com filho de 1 ano, recém-órfão de pai e com uma parte de uma propriedade rural de herança, eis a oportunidade de viver apaixonadamente tech 'n roots, sendo FoodLover.
           Bora alimentar o futuro do mundo! 🚀
         </p>
       </div>
